5ebfebb5827d45173b999688da9de985d9d05e27
adam <kjharper93@gmail.com>
2020-10-15 15:09:26 +0100

    fixed setup file to include examples package

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='pypes',
-    version='0.1.0dev',
-    packages=['pypes',],
-    license='GNU GENERAL PUBLIC LICENSE Version 3',
-    long_description=open('README.md').read(),
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="adam-squared-pypes",
+    version="0.1.0dev1",
+    author="Adam Beecham",
+    author_email="adam.beecham@adamsquared.cloud",
+    description="A framework for building lightweight, stateless data pipelines with python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adam-squared/pypes",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
 )
