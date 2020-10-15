af9b5cbe2504e66b7ccb60ca763ea612c4765dfb
adam <kjharper93@gmail.com>
2020-10-15 13:53:28 +0100

    added basic setup module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pypes',
+    version='0.1.0dev',
+    packages=['pypes',],
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    long_description=open('README.md').read(),
+)
