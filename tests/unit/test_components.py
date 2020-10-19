584c1091b74538dd1402f06eed23e8ce6b887f40
adam <kjharper93@gmail.com>
2020-10-19 14:37:26 +0100

    blackened code

--- tests/unit/test_components.py
+++ tests/unit/test_components.py
@@ -1,47 +1,62 @@
 def test_processor_with_func():
     pass
 
+
 def test_processor_with_lambda():
     pass
+
 
 def test_processor_with_procfn():
     pass
 
+
 def test_proc_piping_with_implicit_relationship():
     pass
+
 
 def test_proc_piping_with_expicit_relationship():
     pass
 
+
 def test_iterate_relationship():
     pass
+
 
 def test_get_relationship_dests():
     pass
 
+
 def test_funnel_piping():
     pass
+
 
 def test_pipeline_with_simple_runner():
     pass
 
+
 def test_pipeline_with_unbound_source():
     pass
+
 
 def test_pipeline_with_bound_source():
     pass
 
+
 def test_pipeline_with_multi_source():
     pass
+
 
 def test_pipeline_with_unused_relationship():
     pass
 
+
 def test_pipeline_with_non_iter_processor():
     pass
 
+
 def test_pipeline_with_multi_sink():
     pass
+
 
 def test_pipeline_with_loop():
     pass
