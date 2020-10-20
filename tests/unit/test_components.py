2f300272c3a4d2f0db02e6edd813c08dd7f5ee0a
adam <kjharper93@gmail.com>
2020-10-20 13:03:30 +0100

    finished initial unit test suite

--- tests/unit/test_components.py
+++ tests/unit/test_components.py
@@ -59,6 +59,8 @@ def test_relationship_or_operator_should_append_other_to_dest():
 
 
 def test_funnel_or_operator_should_append_other_to_all_inputs():
+    # when piping a funnel to another item, it should be appended
+    # to the destinations of all relationships in the funnel
     in_rel_1 = pypes.Relationship()
     in_rel_2 = pypes.Relationship()
     in_rel_3 = pypes.Relationship()
@@ -75,6 +77,8 @@ def test_funnel_or_operator_should_append_other_to_all_inputs():
 
 
 def test_funnel_or_operator_should_return_other():
+    # when piping a funnel to another item, the other item
+    # should be returned
     in_rel_1 = pypes.Relationship()
     in_rel_2 = pypes.Relationship()
     in_rel_3 = pypes.Relationship()
@@ -84,3 +88,94 @@ def test_funnel_or_operator_should_return_other():
     out = funnel | "other"
 
     assert out == "other"
+
+def test_processor_should_run_lambda():
+    # a processor should execute a lambda passed to the
+    # constructor when the process method is called
+    with pypes.Processor(lambda:(yield 'hello world')) as p:
+        generator = p.process()
+        out = next(generator)
+
+        assert out == 'hello world'
+
+def test_processor_should_run_function():
+    # a processor should execute a function passed to the
+    # constructor when the process method is called
+    def get_message(data):
+        yield f'the input was {data}'
+
+    with pypes.Processor(get_message) as p:
+        generator = p.process('hello')
+        out = next(generator)
+
+        assert out == 'the input was hello'
+
+def test_processor_should_run_proc_fn():
+    # a processor should execute a function passed to the
+    # constructor when the process method is called
+    class MessageFn(pypes.ProcFn):
+        def process(self, data):
+            yield f'the input was {data}'
+
+    with pypes.Processor(MessageFn()) as p:
+        generator = p.process('hello')
+        out = next(generator)
+
+        assert out == 'the input was hello'
+
+def test_proccessor_or_operator_should_append_other_to_success_relationship():
+    # when piping two processors, the destination processor should be appended
+    # to the success relationship of the source processor
+    src = pypes.Processor(lambda: (yield 'hello'))
+    dest = pypes.Processor(lambda: (yield 'goodbye'))
+
+    src | dest
+
+    assert dest in src.relationships['success'].destinations
+
+def test_processor_or_operator_should_return_other():
+    # when piping two processors, the destination processor should be
+    # returned
+    src = pypes.Processor(lambda: (yield 'hello'))
+    dest = pypes.Processor(lambda: (yield 'goodbye'))
+
+    out = src | dest
+    assert out == dest
+
+def test_processor_rshift_should_return_named_relationship():
+    # when using the rshift operator with a processor, a relationship
+    # with the provided name should be returned
+    proc = pypes.Processor(lambda: (yield 'hello'))
+    failed = proc >> 'failed'
+
+    assert proc.relationships['failed'] == failed
+
+def test_runner_should_run_all_piped_processors():
+    # the simple runner should execute all processors that
+    # have been piped together
+    exp_outs = ['foofoo', 'barbar', 'bazbaz']
+    outs = []
+    src = pypes.Processor(lambda: iter([('success', x) for x in ['foo', 'bar', 'baz']]))
+    trn = pypes.Processor(lambda x: (yield 'success', x + x))
+    dest = pypes.Processor(lambda x: outs.append(x))
+
+    src | trn | dest
+
+    runner = pypes.SimpleRunner()
+    runner.run([src])
+
+    assert outs == exp_outs
+
+def test_pipeline_should_run_all_piped_processors():
+    # the pipeline should execute all processors piped into the pipeline
+    exp_outs = ['foofoo', 'barbar', 'bazbaz']
+    outs = []
+
+    with pypes.Pipeline() as p:
+        (p
+            | pypes.Processor(lambda: iter([('success', x) for x in ['foo', 'bar', 'baz']]))
+            | pypes.Processor(lambda x: (yield 'success', x + x))
+            | pypes.Processor(lambda x: outs.append(x))
+        )
+
+    assert outs == exp_outs
